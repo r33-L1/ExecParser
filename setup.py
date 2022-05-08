5ef16ad683e19b7cfd4bc1e8d1db233d05a309ef
r33-L1 <study.white.fox@gmail.com>
2022-05-08 10:09:28 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ExecParser',
+    version='0.0.1',
+    packages=['ExecParser'],
+    url='https://github.com/r33-L1/ExecParser',
+    license='MIT License',
+    author='Vladislav Burtsev',
+    author_email='study.white.fox@gmail.com',
+    description='Parse PE, ELF, and Mach-O using lief'
+)
