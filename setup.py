f7b902b9ff65ebcaf63927b09380450f619fc8a2
r33-L1 <study.white.fox@gmail.com>
2022-05-09 17:09:05 +0300

    transformed to build

--- setup.py
+++ setup.py
@@ -1,12 +1,40 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+import re
+
+VERSIONFILE = "ExecParser/_version.py"
+verstrline = open(VERSIONFILE, "rt").read()
+VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, verstrline, re.M)
+if mo:
+    verstr = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
+
+ep = {
+    'console_scripts': [
+        'ExecParser = ExecParser.__main__:main',
+    ],
+}
 
 setup(
-    name='ExecParser',
-    version='0.0.1',
-    packages=['ExecParser'],
-    url='https://github.com/r33-L1/ExecParser',
-    license='MIT License',
+    name="ExecParser",
+    version=verstr,
     author='Vladislav Burtsev',
     author_email='study.white.fox@gmail.com',
-    description='Parse PE, ELF, and Mach-O using lief'
+    packages=find_packages(),
+    include_package_data=True,
+    url='https://github.com/r33-L1/ExecParser',
+    zip_safe=True,
+    license='MIT License',
+    description='Parse PE, ELF, and Mach-O using lief',
+    python_requires='>=3.6',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+
+    install_requires=[
+        'lief>=0.12.0',
+    ],
 )
